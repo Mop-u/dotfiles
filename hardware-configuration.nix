ccfaa6c1d34fd0b046e0f33732d499cc9da3e856
Quinn Unger <quinn.unger@pm.me>
2024-08-22 19:06:39 +0100

    increase AC performance in tlp settings

--- hardware-configuration.nix
+++ hardware-configuration.nix
@@ -83,6 +83,15 @@
     services.tlp = {
         enable = true; # laptop power saving etc
         settings = {
+            PLATFORM_PROFILE_ON_AC = "performance";
+            PLATFORM_PROFILE_ON_BAT = "low-power";
+            CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
+            CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
+            CPU_SCALING_GOVERNOR_ON_AC = "performance";
+            CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+            CPU_BOOST_ON_AC = 1;
+            CPU_BOOST_ON_BAT = 0;
+
             START_CHARGE_THRESH_BAT0 = 40;
             STOP_CHARGE_THRESH_BAT0 = 80;
             NATACPI_ENABLE = 1; # battery care driver
