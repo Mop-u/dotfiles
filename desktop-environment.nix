fce214de4ae4724f818fd01c4028cbf2f1e58e13
Quinn Unger <quinn.unger@pm.me>
2024-08-30 14:59:55 +0100

    use latest xdg-desktop-portal-hyprland

--- desktop-environment.nix
+++ desktop-environment.nix
@@ -35,6 +35,7 @@
         hyprland = {
             enable = true;
             package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
+            portalPackage = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
         };
         steam = {
             enable = true;
