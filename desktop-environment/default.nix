84038985c58bf1725b82e06e54d756b47a29925f
Quinn Unger <quinn.unger@pm.me>
2024-09-13 18:43:33 +0100

    apply theme to vesktop

--- desktop-environment/default.nix
+++ desktop-environment/default.nix
@@ -231,6 +231,15 @@
                 }
             '';
         };
+        home.file.vesktop = {
+            enable = true;
+            executable = false;
+            target = "/home/${target.userName}/.config/vesktop/settings/quickCss.css";
+            text = ''
+                @import url("https://catppuccin.github.io/discord/dist/catppuccin-${target.catppuccin.flavor}.theme.css");
+                @import url("https://catppuccin.github.io/discord/dist/catppuccin-${target.catppuccin.flavor}-${target.catppuccin.accent}.theme.css");
+            '';
+        };
 
         programs = {
             bemenu = {
