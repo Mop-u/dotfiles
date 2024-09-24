0933827f0d71f82574918f2ce1aa62749e695a22
Quinn Unger <quinn.unger@pm.me>
2024-09-24 21:19:02 +0100

    yure: configure thinkpad fan control

--- target/yure/hardware-configuration.nix
+++ target/yure/hardware-configuration.nix
@@ -84,4 +84,30 @@ in {
             NATACPI_ENABLE = 1; # battery care driver
         };
     };
+    services.thinkfan = {
+        enable = true;
+        levels = [
+            [0 
+                00 55
+            ]
+            [1
+                48 60
+            ]
+            [2
+                50 61
+            ]
+            [3
+                52 63
+            ]
+            [6
+                56 65
+            ]
+            [7
+                60 85
+            ]
+            ["level disengaged"
+                80 32767
+            ]
+        ];
+    };
 }
