318addd4be9b270c100289f3b8828d5a1c252e7c
Quinn Unger <quinn.unger@pm.me>
2024-09-26 13:31:25 +0100

    use builtins.toJSON to build the sublime configuration

--- desktop-environment/sublime4.nix
+++ desktop-environment/sublime4.nix
@@ -70,97 +70,83 @@ in {
                 enable = true;
                 executable = false;
                 target = "${stextCfg}/Preferences.sublime-settings";
-                text = ''
-                {
-                    "ignored_packages":
-                    [
-                        "Vintage",
-                    ],
-                    "font_size": ${if target.text.smallTermFont then "11" else "12"},
-                    "translate_tabs_to_spaces": true,
-                    "index_files": false,
-                    "hardware_acceleration": "${if target.legacyGpu then "none" else "opengl"}",
-                    "theme": "Adaptive.sublime-theme",
-                    "color_scheme": "Catppuccin ${target.helper.capitalize target.style.catppuccin.flavor}.sublime-color-scheme",
-                    "update_check": false,
-                    "sublime_merge_path": ${if target.helper.isInstalled pkgs.sublime-merge then "\"${pkgs.sublime-merge}/bin/sublime_merge\"" else "null"},
-                }
-                '';
+                text = builtins.toJSON {
+                    ignored_packages = ["Vintage"];
+                    font_size = if target.text.smallTermFont then 11 else 12;
+                    translate_tabs_to_spaces = true;
+                    index_files = true;
+                    hardware_acceleration = if target.legacyGpu then "none" else "opengl";
+                    theme = "Adaptive.sublime-theme";
+                    color_scheme = "Catppuccin ${target.helper.capitalize target.style.catppuccin.flavor}.sublime-color-scheme";
+                    update_check = false;
+                    sublime_merge_path = if target.helper.isInstalled pkgs.sublime-merge then "\"${pkgs.sublime-merge}/bin/sublime_merge\"" else null;
+                };
             };
             stextSublimeLinterCfg = {
                 enable = true;
                 executable = false;
                 target = "${stextCfg}/SublimeLinter.sublime-settings";
-                text = ''
-                {
-                    "no_column_highlights_line": true,
-                    "debug": false,
-                    "linters":
-                    {
-                        "verilator": {
-                            "disable": ${if target.helper.isInstalled pkgs.verilator then "false" else "true"},
-                            "lint_mode": "load_save",
-                            "styles" : [
+                text = builtins.toJSON {
+                    no_column_highlights_line = true;
+                    debug = false;
+                    linters = {
+                        verilator = {
+                            disable = if target.helper.isInstalled pkgs.verilator then false else true;
+                            lint_mode = "load_save";
+                            styles = [
                                 {
-                                    "types": ["warning"],
-                                    "mark_style": "squiggly_underline",
-                                    "icon": "Packages/SublimeLinter/gutter-themes/Default/cog.png"
-                                },
-                                {
-                                    "types": ["error"],
-                                    "mark_style": "fill",
-                                    "icon": "Packages/SublimeLinter/gutter-themes/Default/cog.png"
+                                    types = ["warning"];
+                                    mark_style = "squiggly_underline";
+                                    icon = "Packages/SublimeLinter/gutter-themes/Default/cog.png";
                                 }
-                            ],
-                            "args": [
-                                "--error-limit",
-                                "500",
-                                "--default-language",
-                                "1800-2005",
-                                "-Wall",
-                            ],
+                                {
+                                    types = ["error"];
+                                    mark_style = "fill";
+                                    icon = "Packages/SublimeLinter/gutter-themes/Default/cog.png";
+                                }
+                            ];
+                            args = [
+                                "--error-limit"
+                                "500"
+                                "--default-language"
+                                "1800-2005"
+                                "-Wall"
+                            ];
 
-                            "verilator_version"  : 5,
-                            "use_multiple_source": true,
-                            "search_project_path": true,
+                            verilator_version = 5;
+                            use_multiple_source = true;
+                            search_project_path = true;
 
-                            // windows subsystem for linux (wsl verilator_bin)
-                            "use_wsl": false,
+                            use_wsl = false;
 
-                            // additional option to filter file type
-                            "extension": [
-                                ".v", ".sv", ".svh", ".vh"
-                            ],
-                        }
-                    }
-                }
-                '';
+                            extension = [
+                                ".v" ".sv" ".svh" ".vh"
+                            ];
+                        };
+                    };
+                };
             };
             stextLSPCfg = {
                 enable = true;
                 executable = false;
                 target = "${stextCfg}/LSP.sublime-settings";
-                text = ''
-                {
-                    "clients": {
-                        "verilbe": {
-                            "enabled": ${if target.helper.isInstalled pkgs.verible then "true" else "false"},
-                            "command": [
-                                "verible-verilog-ls",
+                text = builtins.toJSON {
+                    clients = {
+                        verilbe = {
+                            enabled = if target.helper.isInstalled pkgs.verible then true else false;
+                            command = [
+                                "verible-verilog-ls"
                                 "--rules_config_search"
-                            ],
-                            "selector": "source.systemverilog"
-                        },
-                        "nil": {
-                            "enabled": ${if target.helper.isInstalled pkgs.nil then "true" else "false"},
-                            "command": [
-                                "nil"
-                            ],
-                            "selector": "source.nix"
-                        }
-                    }
-                }
-                '';
+                            ];
+                            selector = "source.systemverilog";
+                        };
+                        nil = {
+                            enabled = if target.helper.isInstalled pkgs.nil then true else false;
+                            command = ["nil"];
+                            selector = "source.nix";
+                        };
+                    };
+                };
             };
         };
     };
