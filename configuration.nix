852d1fc86c74041a266133f2ac7b85712f89c7db
Quinn Unger <quinn.unger@pm.me>
2024-10-03 14:15:27 +0100

    patch agetty paths for kmscon systemd services

--- configuration.nix
+++ configuration.nix
@@ -90,7 +90,7 @@
 
     console.keyMap = target.input.keyLayout;
 
-    # more feature-rich tty
+    # tty with modern font rendering
     # https://github.com/Aetf/kmscon
     nixpkgs.overlays = [
         (final: prev: {
@@ -111,7 +111,62 @@
                     mesa
                 ];
 
-                patches = [./kmscon-systemd-path.patch];
+                patches = [
+                    (pkgs.writeTextFile {
+                        name = "meson.build.patch";
+                        text = ''
+                            diff --git a/meson.build b/meson.build
+                            index 964b44b..fc043c7 100644
+                            --- a/meson.build
+                            +++ b/meson.build
+                            @@ -39,7 +39,7 @@ mandir = get_option('mandir')
+                             moduledir = get_option('libdir') / meson.project_name()
+                             
+                             systemd_deps = dependency('systemd', required: false)
+                            -systemdsystemunitdir = systemd_deps.get_variable('systemdsystemunitdir', default_value: get_option('libdir') / 'systemd/system')
+                            +systemdsystemunitdir = get_option('libdir') / 'systemd/system'
+                             
+                             #
+                             # Required dependencies
+                        '';
+                    })
+                    (pkgs.writeTextFile {
+                        name = "kmscon.service.in.patch";
+                        text = ''
+                            diff --git a/docs/kmscon.service.in b/docs/kmscon.service.in
+                            index ad5600d..664bbfb 100644
+                            --- a/docs/kmscon.service.in
+                            +++ b/docs/kmscon.service.in
+                            @@ -6,7 +6,7 @@ After=systemd-user-sessions.service
+                             After=rc-local.service
+                             
+                             [Service]
+                            -ExecStart=@bindir@/kmscon --login -- /sbin/agetty -o '-p -- \\u' --noclear -- -
+                            +ExecStart=@bindir@/kmscon --login -- ${pkgs.util-linux}/bin/agetty -o '-p -- \\u' --noclear -- -
+                             
+                             [Install]
+                             WantedBy=multi-user.target
+                         '';
+                    })
+                    (pkgs.writeTextFile {
+                        name = "kmsconvt@.service.in.patch";
+                        text = ''
+                            diff --git a/docs/kmsconvt@.service.in b/docs/kmsconvt@.service.in
+                            index a496e26..18a1f93 100644
+                            --- a/docs/kmsconvt@.service.in
+                            +++ b/docs/kmsconvt@.service.in
+                            @@ -38,7 +38,7 @@ IgnoreOnIsolate=yes
+                             ConditionPathExists=/dev/tty0
+                             
+                             [Service]
+                            -ExecStart=@bindir@/kmscon --vt=%I --seats=seat0 --no-switchvt --login -- /sbin/agetty -o '-p -- \\u' --noclear -- -
+                            +ExecStart=@bindir@/kmscon --vt=%I --seats=seat0 --no-switchvt --login -- ${pkgs.util-linux}/bin/agetty -o '-p -- \\u' --noclear -- -
+                             UtmpIdentifier=%I
+                             TTYPath=/dev/%I
+                             TTYReset=yes
+                        '';
+                    })
+                ];
 
                 env.NIX_CFLAGS_COMPILE = "-O" # _FORTIFY_SOURCE requires compiling with optimization (-O)
                     # https://github.com/Aetf/kmscon/issues/49
