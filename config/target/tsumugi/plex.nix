92d91cc2b9835ec7817b29b20f75f39c12f78681
Quinn Unger <quinn.unger@pm.me>
2024-11-25 15:43:11 +0000

    test container port forwarding

--- config/target/tsumugi/plex.nix
+++ config/target/tsumugi/plex.nix
@@ -13,10 +13,6 @@
             })
         ];
     };
-    services.sonarr = {
-        enable = true;
-        openFirewall = true; # 8989
-    };
     services.radarr = {
         enable = true;
         openFirewall = true; # 7878
@@ -44,5 +40,54 @@
     #            "Label"
     #        ];
     #    };
+    #};
+
+
+    networking.firewall.allowedTCPPorts = [ 8998 ];
+
+    networking.nat = {
+        enable = true;
+        enableIPv6 = true;
+        internalInterfaces = ["ve-+"];
+        externalInterface = "enp6s0";
+    };
+
+    containers.sonarrStd = {
+        
+        autoStart = true;
+        privateNetwork = true;
+        hostAddress = "192.168.168.10";
+        localAddress = "192.168.168.11";
+        hostAddress6 = "fc00::10";
+        localAddress6 = "fc00::11";
+
+        forwardPorts = [{
+            containerPort = 8989;
+            hostPort = 8998;
+            protocol = "tcp";
+        }];
+
+        config = {
+            system.stateVersion = target.stateVer;
+            networking = {
+                firewall.enable = true;
+                # Use systemd-resolved inside the container
+                # Workaround for bug https://github.com/NixOS/nixpkgs/issues/162686
+                useHostResolvConf = lib.mkForce false;
+            };
+            services.resolved.enable = true;
+
+            services.sonarr = {
+                enable = true;
+                openFirewall = true;
+            };
+        };
+    };
+
+    #containers.sonarrAnime = {
+    #};
+    #containers.radarrStandard = {
+    #};
+    #containers.radarrAnime = {
     #};
 }
