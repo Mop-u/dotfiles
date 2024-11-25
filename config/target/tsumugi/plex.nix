6b68f30428aa2b0240dc272fe0426bb2292c7173
Quinn Unger <quinn.unger@pm.me>
2024-11-25 21:08:24 +0000

    declarative config seems to be broken, disabling.

--- config/target/tsumugi/plex.nix
+++ config/target/tsumugi/plex.nix
@@ -57,25 +57,18 @@ in {
         openFirewall = true; # 5055
     };
     
-    sops.secrets."tsumugi/delugePass" = {};
-    sops.templates.delugeAuthFile = {
-        owner = "deluge";
-        content = ''
-            localclient:${config.sops.placeholder."tsumugi/delugePass"}:10
-        '';
-    };
+    #sops.secrets."tsumugi/delugePass" = {};
+    #sops.templates.delugeAuthFile = {
+    #    owner = "deluge";
+    #    content = ''
+    #        localclient:${config.sops.placeholder."tsumugi/delugePass"}:10
+    #    '';
+    #};
 
     services.deluge = {
         enable = true;
-        declarative = true;
         web.enable = true;
         web.openFirewall = true; # 8112
-        config = {
-            enabled_plugins = [
-                "Label"
-            ];
-        };
-        authFile = config.sops.templates.delugeAuthFile.path;
     };
 
     networking.nat = {
