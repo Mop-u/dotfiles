5525b6a86e11bb6a26e64d2e2b0d8974ee68368a
Quinn Unger <quinn.unger@pm.me>
2024-12-02 13:30:51 +0000

    add ups monitoring

---
+++ config/target/tsumugi/ups.nix
@@ -0,0 +1,45 @@
+{ inputs, config, pkgs, lib, target, ... }:
+{
+    sops.secrets."tsumugi/nutPass" = {};
+    power.ups = {
+        enable = true;
+        mode = "netserver";
+        openFirewall = true;
+        users.powercoolmon = {
+            passwordFile = config.sops.secrets."tsumugi/nutPass".path;
+            upsmon = "primary";
+        };
+        upsd.listen = [{
+            address = "10.0.4.2";
+            port = 3493;
+        }];
+        upsmon = {
+            settings.NOTIFYFLAG = [
+                ["COMMOK"  "SYSLOG"]
+                ["COMMBAD" "SYSLOG"]
+                ["NOCOMM"  "SYSLOG"]
+            ];
+            monitor.powercool = {
+                user = "powercoolmon";
+                type = "primary";
+                system = "powercool@localhost:3493";
+                powerValue = 1;
+                passwordFile = config.sops.secrets."tsumugi/nutPass".path;
+            };
+        };
+        ups.powercool = {
+            description = "Powercool UPS 1500VA";
+            driver = "nutdrv_qx";
+            port = "auto";
+            directives = [
+                "vendorid = 0001"
+                "productid = 0000"
+                "product = \"MEC0003\""
+                "protocol = hunnox"
+                "langid_fix = 0x409"
+                "novendor"
+                "noscanlangid"
+            ];
+        };
+    };
+}
