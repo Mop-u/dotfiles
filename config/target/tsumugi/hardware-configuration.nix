ba1bca6dc32cbf1ed083900f627a1b45fbeaf6ac
Quinn Unger <quinn.unger@pm.me>
2024-12-08 03:08:44 +0000

    use default linux kernel

--- config/target/tsumugi/hardware-configuration.nix
+++ config/target/tsumugi/hardware-configuration.nix
@@ -16,7 +16,7 @@
 
     boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "nvme" "usbhid" "usb_storage" "sd_mod" ];
     boot.initrd.kernelModules = [ ];
-    boot.kernelPackages = pkgs.linuxPackages_zen;
+    #boot.kernelPackages = pkgs.linuxPackages_latest;
     boot.kernelModules = [ "kvm-intel" ];
     boot.extraModulePackages = [ ];
 
