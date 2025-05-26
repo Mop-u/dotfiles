6e884a871671be559f30af6bddb35c0a7538fcea
Quinn Unger <quinn.unger@pm.me>
2025-05-26 12:51:46 +0100

    fixup wireguard config

--- hosts/tsumugi/reverseProxy.nix
+++ hosts/tsumugi/reverseProxy.nix
@@ -7,24 +7,23 @@
 }:
 {
     sops.secrets."tsumugi/wgpk" = { };
-    sops.secrets."ochiai/ip" = { };
-    sops.templates.ochiaiWgQuick.content = ''
-        [Interface]
-        ListenPort = 9546
-        Address = 10.0.0.2/24
-        PrivateKey = ${config.sops.placeholder."tsumugi/wgpk"}
-        Table = 1234
-        PostUp = ip rule add pref 500 from 10.0.0.2 lookup 1234
-        PostDown = ip rule delete pref 500
-
-        [Peer]
-        PublicKey = UoMs4Mtp59wHfJwGLygNe01W3vrBXEevEElZ+rs27Xs=
-        AllowedIPs = 0.0.0.0/0
-        Endpoint = ${config.sops.placeholder."ochiai/ip"}:5964
-        PersistentKeepalive = 25
-    '';
     networking.firewall.allowedUDPPorts = [ 9546 ];
-    networking.wg-quick.interfaces.ochiai.configFile = config.sops.templates.ochiaiWgQuick.path;
+    networking.wg-quick.interfaces.ochiai = {
+        listenPort = 9546;
+        address = [ "10.0.0.2/24" ];
+        privateKeyFile = config.sops.secrets."tsumugi/wgpk".path;
+        table = "1234";
+        postUp = "ip rule add pref 500 from 10.0.0.2 lookup 1234";
+        postDown = "ip rule delete pref 500";
+        peers = [
+            {
+                publicKey = "UoMs4Mtp59wHfJwGLygNe01W3vrBXEevEElZ+rs27Xs=";
+                allowedIPs = [ "0.0.0.0/0" ];
+                endpoint = "ochiai.moppu.dev:5964";
+                persistentKeepalive = 25;
+            }
+        ];
+    };
 
     sops.secrets."tsumugi/cloudflare" = {
         #owner = "cloudflared";
