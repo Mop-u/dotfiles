7f25e87d410931a0de3129f5523e76e694c112ea
Quinn Unger <quinn.unger@pm.me>
2025-11-07 13:04:20 +0000

    yuhata: clean up network mount

--- hosts/yuhata/networkMounts.nix
+++ hosts/yuhata/networkMounts.nix
@@ -6,43 +6,26 @@
     ...
 }:
 let
-    useNFS = false;
-    hardMount = true;
     mntBenisuzume = name: {
-        fsType = if useNFS then "nfs" else "cifs";
-        device = if useNFS then "10.0.4.3:/var/nfs/shared/${name}" else "//10.0.4.3/${name}";
-        options =
-            (
-                if useNFS then
-                    [
-                        #"nfsvers=3"
-                        "intr"
-                        "nolock"
-                        "retry=infinity"
-                        "timeo=60"
-                    ]
-                else
-                    [
-                        "credentials=${config.sops.secrets."benisuzume/cifs".path}"
-                        "x-systemd.automount"
-                        "x-systemd.idle-timeout=30"
-                        "nofail"
-                        "uid=${config.sidonia.userName}"
-                        "gid=users"
-                        "rw"
-                        "rwpidforward"
-                        "exec"
-                    ]
-            )
-            ++ [
-                (if hardMount then "hard" else "soft")
-                "fsc"
-            ];
+        fsType = "cifs";
+        device = "//10.0.4.3/${name}";
+        options = [
+            "credentials=${config.sops.secrets."benisuzume/cifs".path}"
+            "x-systemd.automount"
+            "x-systemd.idle-timeout=30"
+            "nofail"
+            "uid=${config.sidonia.userName}"
+            "gid=users"
+            "rw"
+            "rwpidforward"
+            "exec"
+            "hard"
+            "fsc"
+        ];
     };
 in
 {
     services.rpcbind.enable = true;
-    boot.supportedFilesystems = [ "nfs" ];
     environment.systemPackages = [ pkgs.cifs-utils ];
 
     sops.secrets."benisuzume/cifs" = { };
