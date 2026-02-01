77045c66e9d42bbcad677bb6e4953db14163478f
Quinn Unger <quinn.unger@pm.me>
2026-02-01 03:40:08 +0000

    packaged veadotube (bring-your-own zip file)

--- hosts/yuhata/home.nix
+++ hosts/yuhata/home.nix
@@ -7,9 +7,12 @@
 }:
 {
     home.packages = with pkgs; [
+        nix-index
         bs-manager
         via
         qmk
         limo
+        (pkgs.callPackage ./packages/veadotube.nix {})
     ];
+    services.hyprpolkitagent.enable = true;
 }
