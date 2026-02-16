398c509fcc18b73316f46146d08caa4589901096
Quinn Unger <quinn.unger@pm.me>
2026-02-16 00:35:32 +0000

    yuhata: configure gamemode

--- hosts/yuhata/default.nix
+++ hosts/yuhata/default.nix
@@ -24,7 +24,6 @@
             };
         };
     };
-
     networking.hostName = "yuhata";
     sidonia = {
         userName = "midorikawa";
@@ -95,12 +94,28 @@
         pkgs.openssl_1_1.name # for openrgb service
     ];
 
+    users.users.midorikawa.extraGroups = [ "gamemode" ];
+    programs.gamemode = {
+        enable = true;
+        enableRenice = true;
+        settings = {
+            # man gamemoded(8)
+            general = {
+                renice = 10;
+            };
+            gpu = {
+                apply_gpu_optimisations = "accept-responsibility";
+                gpu_device = 1;
+                nv_powermizer_mode = 1;
+                #nv_mem_clock_mhz_offset = 0;
+            };
+        };
+    };
     programs = {
         sleepy-launcher.enable = true;
         coolercontrol.enable = true;
         steam.remotePlay.openFirewall = true;
         gamescope.enable = true;
-        gamemode.enable = true;
     };
     hardware.keyboard.qmk.enable = true;
     services.udev.packages = [
