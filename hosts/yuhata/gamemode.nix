57d7a259ca58c67b797f14428375d2d3c68f0726
Quinn Unger <quinn.unger@pm.me>
2026-02-16 20:54:40 +0000

    use latest gamemode

--- hosts/yuhata/gamemode.nix
+++ hosts/yuhata/gamemode.nix
@@ -7,6 +7,20 @@
 }:
 {
     users.users.midorikawa.extraGroups = [ "gamemode" ];
+    nixpkgs.overlays = [
+        (final: prev: {
+            gamemode = prev.gamemode.overrideAttrs (
+                finalAttrs: prevAttrs: {
+                    src = final.fetchFromGitHub {
+                        owner = "FeralInteractive";
+                        repo = "gamemode";
+                        rev = "f0a569a5199974751a4a75ebdc41c8f0b8e4c909";
+                        hash = "sha256-9DB8iWiyrM4EJ94ERC5SE9acrhqeI00BF1wU0umeNFg=";
+                    };
+                }
+            );
+        })
+    ];
     programs.gamemode = {
         enable = true;
         enableRenice = true;
@@ -17,8 +31,9 @@
             };
             gpu = {
                 apply_gpu_optimisations = "accept-responsibility";
-                #gpu_device = 1;
+                gpu_device = 1;
                 nv_powermizer_mode = 1;
+                #nv_core_clock_mhz_offset = 0;
                 #nv_mem_clock_mhz_offset = 0;
             };
         };
