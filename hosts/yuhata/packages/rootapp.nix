4d3ee6ece10c3b9d6c450e82a1f79c9cb1d3ff7d
Quinn Unger <quinn.unger@pm.me>
2026-02-21 16:18:17 +0000

    install rootapp desktop entry

--- hosts/yuhata/packages/rootapp.nix
+++ hosts/yuhata/packages/rootapp.nix
@@ -10,7 +10,18 @@ let
         url = "https://installer.rootapp.com/installer/Linux/X64/Root.AppImage";
         hash = "sha256-ejlytvqMnsD6UPKMxjstx1oMIbEPAqFR5W983WhEGAM=";
     };
+
+    appimageContents = appimageTools.extract {
+        inherit pname version src;
+        postExtract = ''
+            substituteInPlace $out/Root.desktop --replace-fail 'Exec=Root' 'Exec=rootapp'
+        '';
+    };
 in
 appimageTools.wrapType2 {
     inherit pname version src;
+    extraInstallCommands = ''
+        install -m 444 -D ${appimageContents}/Root.desktop $out/share/applications/Root.desktop
+        install -m 444 -D ${appimageContents}/Root.png $out/share/icons/hicolor/512x512/apps/Root.png
+    '';
 }
