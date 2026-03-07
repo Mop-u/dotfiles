d51ce52894a133bec846c2d1bf249a1be2d2e994
Quinn Unger <quinn.unger@pm.me>
2026-03-07 00:49:56 +0000

    yuhata: use hyprctl for 10bit monitor setting

--- hosts/yuhata/home/monitors.nix
+++ hosts/yuhata/home/monitors.nix
@@ -10,6 +10,9 @@ let
     tv = "d%Q90A";
     zowie = "d%ZOWIE";
     ultrawide = "d%P40w-20";
+    replaceSeps =
+        from: to: str:
+        lib.concatStringsSep to (lib.splitString from str);
 in
 {
     services.shikane = {
@@ -25,13 +28,23 @@ in
                         mode = "1920x1080@60";
                         position = "4800,400";
                     }
-                    {
+
+                    (rec {
                         search = ultrawide;
                         enable = true;
                         scale = 1.07;
                         mode = "5120x2160@74.98";
                         position = "0,0";
-                    }
+                        exec = lib.optional (osConfig.sidonia.desktop.compositor == "hyprland") (
+                            lib.concatStringsSep "," [
+                                "hyprctl keyword monitor desc:Lenovo Group Limited P40w-20"
+                                mode
+                                (replaceSeps "," "x" position)
+                                (lib.strings.floatToString scale)
+                                "bitdepth,10"
+                            ]
+                        );
+                    })
                 ];
             }
             {
