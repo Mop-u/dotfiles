a3dd2a7aedefe5be083e58afcb2a56a9b334dcf6
Quinn Unger <quinn.unger@pm.me>
2026-03-11 01:46:36 +0000

    patch bluetooth

--- hosts/shizuka/hardware-configuration.nix
+++ hosts/shizuka/hardware-configuration.nix
@@ -31,8 +31,10 @@
     nix.settings.substituters = [ "https://attic.xuyh0120.win/lantian" ];
     nix.settings.trusted-public-keys = [ "lantian:EeAUQ+W+6r7EtwnmYjeVwx5kOGEBpjlBfPlzGlTNvHc=" ];
     boot.kernelPackages =
-        inputs.cachyos.legacyPackages.x86_64-linux.linuxPackages-cachyos-latest-lto-x86_64-v3;
-
+        inputs.cachyos.legacyPackages.x86_64-linux.linuxPackages-cachyos-latest-lto-x86_64-v3.override
+            (prev: {
+                patches = (prev.patches or [ ]) ++ [ ./patches/QCA_ROME.patch ];
+            });
     boot.extraModulePackages = [ ];
 
     fileSystems."/" = {
