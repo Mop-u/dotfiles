f8000e3a84349e008d51003e78ba602b4b6fac62
Quinn Unger <quinn.unger@pm.me>
2026-03-23 20:51:36 +0000

    shizuka: enable distributed builds

--- hosts/shizuka/default.nix
+++ hosts/shizuka/default.nix
@@ -15,6 +15,7 @@
     sidonia = {
         userName = "hoshijiro";
         ssh.pubKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGebQDOjFJrLRI/kxToxRVVYZo8GQWhYy/TW/dXoYcQn hoshijiro@shizuka";
+        services.distributedBuilds.client.enable = true;
         desktop = {
             enable = true;
             compositor = "niri";
