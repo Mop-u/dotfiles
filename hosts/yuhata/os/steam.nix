1db923baef34806e3c383f6edd64ef42a2801e84
Quinn Unger <quinn.unger@pm.me>
2026-04-11 17:03:18 +0100

    yuhata/gamescope: increase refresh rate cap to 2x main monitor's refresh

--- hosts/yuhata/os/steam.nix
+++ hosts/yuhata/os/steam.nix
@@ -23,7 +23,7 @@
             WINE_CPU_TOPOLOGY = "16:0,1,2,3,4,5,6,7,16,17,18,19,20,21,22,23"; # use one CCD
             # https://github.com/NixOS/nixpkgs/issues/162562#issuecomment-1523177264
             SDL_VIDEODRIVER = null;
-            SCB_GAMESCOPE_ARGS = "-f -w 5120 -h 2160 -W 5120 -H 2160 -r 75 --force-grab-cursor --backend sdl"; # scopebuddy -w 3200 -h 1800 -W 5120 -H 2160 -F nis  -f -r 150 -e --sdr-gamut-wideness 1 -- env SteamDeck=1
+            SCB_GAMESCOPE_ARGS = "-f -w 5120 -h 2160 -W 5120 -H 2160 -r 150 --force-grab-cursor --backend sdl"; # scopebuddy -w 3200 -h 1800 -W 5120 -H 2160 -F nis  -f -r 150 -e --sdr-gamut-wideness 1 -- env SteamDeck=1
         };
         extraPackages = with pkgs; [
             gamescope
