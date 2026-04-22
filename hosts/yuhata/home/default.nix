b8a48967d8cb69c305983e54c180408bea4f74df
Quinn Unger <quinn.unger@pm.me>
2026-04-22 21:03:00 +0100

    yuhata: don't install rootapp

--- hosts/yuhata/home/default.nix
+++ hosts/yuhata/home/default.nix
@@ -18,7 +18,7 @@
         qmk
         (limo.override { withUnrar = true; })
         veadotube
-        (pkgs.callPackage ../packages/rootapp.nix { })
+        #(pkgs.callPackage ../packages/rootapp.nix { })
         wl-clipboard
     ];
     programs.mangohud.enable = true;
