39e0ef8db5a466c921c6c2e31d02e4e80b082baf
Quinn Unger <quinn.unger@pm.me>
2026-07-14 23:03:15 +0100

    don't use preset

--- hosts/tsumugi/gameServers/palworld.nix
+++ hosts/tsumugi/gameServers/palworld.nix
@@ -9,6 +9,13 @@
   services.steamcmd-servers.palworld = {
     enable = true;
     openFirewall = true;
-    preset = "palworld";
+    appId = 2394010;
+    executable = "PalServer.sh";
+    args = [
+      "-useperfthreads"
+      "-NoAsyncLoadingThread"
+      "-UseMultithreadForDS"
+    ];
+    udpPorts = [ 8211 ];
   };
 }
