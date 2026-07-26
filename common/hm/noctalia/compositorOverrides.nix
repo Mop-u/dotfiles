b1afbeea8bd76a07d9508b625b24cf146e519b80
Quinn Unger <quinn.unger@pm.me>
2026-07-26 17:34:09 +0100

    fix noctalia config

--- common/hm/noctalia/compositorOverrides.nix
+++ common/hm/noctalia/compositorOverrides.nix
@@ -11,12 +11,13 @@ in
 lib.mkIf
   (cfg.desktop.enable && (cfg.desktop.compositor == "niri") && (cfg.desktop.shell == "noctalia"))
   {
+    # https://docs.noctalia.dev/v5/compositor-settings/niri/
     wayland.desktopManager.sidonia.window.decoration.rounding = 20;
     wayland.windowManager.niri.settings = {
       window-rule = [
         { clip-to-geometry = true; }
         {
-          match._props.app-id = "dev.noctalia.Noctalia.Settings";
+          match._props.app-id = "dev.noctalia.Noctalia";
           open-floating = true;
           default-column-width.fixed = 1080;
           default-window-height.fixed = 920;
@@ -24,14 +25,23 @@ lib.mkIf
       ];
       layer-rule = [
         {
-          match._props.namespace._raw = ''r#"^noctalia-backdrop"#'';
+          match._props.namespace = "^noctalia-backdrop";
           place-within-backdrop = true;
         }
-        (lib.mkIf (!cfg.graphics.legacyGpu) {
-          match._props.namespace._raw = ''r#"^noctalia-(bar-main|notification|dock|panel)$"#'';
+      ]
+      ++ (lib.optionals (!cfg.graphics.legacyGpu) [
+        {
+          match._props.namespace._raw = ''"^noctalia-(bar-[^\"]+|notification|dock|panel|attached-panel|osd)$"'';
           background-effect.xray = false;
-        })
-      ];
+        }
+        {
+          match._props.namespace = "noctalia-window-switcher";
+          background-effect = {
+            blur = true;
+            xray = false;
+          };
+        }
+      ]);
       debug.honor-xdg-activation-with-invalid-serial = [ ];
     };
   }
