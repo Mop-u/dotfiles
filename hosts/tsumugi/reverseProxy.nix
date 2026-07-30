e116e5d6b293929b996bb733390626c1cfba3294
Quinn Unger <quinn.unger@pm.me>
2026-07-30 16:35:17 +0100

    setup tsumugi with netbird self-hosted

--- hosts/tsumugi/reverseProxy.nix
+++ hosts/tsumugi/reverseProxy.nix
@@ -37,14 +37,29 @@
   };
   services.netbird = {
     useRoutingFeatures = "both";
-    clients.wt0 = {
-      port = 51821;
-      login = {
-        enable = true;
-        setupKeyFile = "${pkgs.writeText "one-time-key" "5EA0A6CF-1610-4E9A-9AC3-DD0F139A1928"}";
+    clients = {
+      sidonia = {
+        port = 51820;
+        login = {
+          enable = true;
+          setupKeyFile = "${pkgs.writeText "one-time-key" "55E39BBC-B480-4272-9577-B7046E432A3F"}";
+        };
+        environment = {
+          NB_MANAGEMENT_URL = "https://netbird.moppu.dev";
+          NB_ADMIN_URL = "https://netbird.moppu.dev";
+        };
+        openFirewall = true;
+        openInternalFirewall = true;
       };
-      openFirewall = true;
-      openInternalFirewall = true;
+      wt0 = {
+        port = 51821;
+        login = {
+          enable = true;
+          setupKeyFile = "${pkgs.writeText "one-time-key" "5EA0A6CF-1610-4E9A-9AC3-DD0F139A1928"}";
+        };
+        openFirewall = true;
+        openInternalFirewall = true;
+      };
     };
   };
 }
