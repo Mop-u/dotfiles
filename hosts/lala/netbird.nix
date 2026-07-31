95553cee4ba9ae70e3b516149aac7b89331c5982
Quinn Unger <quinn.unger@pm.me>
2026-07-31 22:19:44 +0100

    setup reverse proxy

--- hosts/lala/netbird.nix
+++ hosts/lala/netbird.nix
@@ -6,16 +6,20 @@
   ...
 }:
 let
+  unstable = inputs.unstable.legacyPackages.${pkgs.stdenv.hostPlatform.system};
   domain = "moppu.dev";
   netbirdDomain = "netbird.${domain}";
   netbirdUrl = "https://${netbirdDomain}";
   stateDir = "/var/lib/netbird-mgmt";
   dashboardPort = 1234;
   relayPort = 33080;
+  proxyPort = 8443;
   coturnPass = config.sops.secrets."lala/netbird/coturnPass".path;
   dataStoreKey = config.sops.secrets."lala/netbird/dataStoreKey".path;
   relaySecret = config.sops.secrets."lala/netbird/relaySecret".path;
   relaySecretEnv = config.sops.secrets."lala/netbird/relaySecretEnv".path;
+  proxySecret = config.sops.secrets."lala/netbird/proxySecret".path;
+  proxySecretEnv = config.sops.secrets."lala/netbird/proxySecretEnv".path;
   idpKey = config.sops.secrets."lala/netbird/idpKey".path;
 in
 {
@@ -24,6 +28,8 @@ in
     "lala/netbird/dataStoreKey" = { };
     "lala/netbird/relaySecret" = { };
     "lala/netbird/relaySecretEnv" = { };
+    "lala/netbird/proxySecret" = { };
+    "lala/netbird/proxySecretEnv" = { };
     "lala/netbird/idpKey" = { };
   };
 
@@ -64,6 +70,20 @@ in
       log.level = "INFO";
     };
     dynamicConfigOptions = {
+      tcp = {
+        routers = {
+          proxy-passthrough = {
+            rule = "HostSNI(`*`)";
+            entryPoints = "websecure";
+            tls.passthrough = true;
+            service = "proxy-tls";
+            priority = 1;
+          };
+        };
+        services.proxy-tls.loadBalancer.servers = [
+          { address = "localhost:${toString proxyPort}"; }
+        ];
+      };
       http = {
         routers = {
           netbird-dashboard = {
@@ -71,7 +91,7 @@ in
             entryPoints = "websecure";
             tls.certResolver = "letsencrypt";
             service = "netbird-dash";
-            priority = 1;
+            priority = 10;
           };
           netbird-signal = {
             rule = "Host(`${netbirdDomain}`) && PathPrefix(`/ws-proxy/signal`)";
@@ -198,45 +218,93 @@ in
     };
   };
 
-  systemd.services.netbird-relay = {
-    enable = true;
-    description = "The relay service for Netbird, a wireguard VPN";
-    documentation = [ "https://netbird.io/docs/" ];
-    after = [ "network.target" ];
-    wantedBy = [ "multi-user.target" ];
-    environment = {
-      NB_LOG_LEVEL = "info";
-      NB_LISTEN_ADDRESS = ":${toString relayPort}";
-      NB_EXPOSED_ADDRESS = "rels://${netbirdDomain}:${toString relayPort}";
-      NB_HEALTH_LISTEN_ADDRESS = ":9999";
-      NB_METRICS_PORT = "9998";
-      NB_TLS_CERT_FILE = "/var/lib/acme/${netbirdDomain}/fullchain.pem";
-      NB_TLS_KEY_FILE = "/var/lib/acme/${netbirdDomain}/key.pem";
-    };
-    serviceConfig = {
-      EnvironmentFile = relaySecretEnv;
-      ExecStart = lib.getExe pkgs.netbird-relay;
-      Restart = "always";
+  systemd.services = {
+    netbird-relay = {
+      enable = true;
+      description = "The relay service for Netbird, a wireguard VPN";
+      documentation = [ "https://netbird.io/docs/" ];
+      after = [
+        "network.target"
+        "netbird-management.service"
+      ];
+      wantedBy = [ "multi-user.target" ];
+      environment = {
+        NB_LOG_LEVEL = "info";
+        NB_LISTEN_ADDRESS = ":${toString relayPort}";
+        NB_EXPOSED_ADDRESS = "rels://${netbirdDomain}:${toString relayPort}";
+        NB_HEALTH_LISTEN_ADDRESS = ":9999";
+        NB_METRICS_PORT = "9998";
+        NB_TLS_CERT_FILE = "/var/lib/acme/${netbirdDomain}/fullchain.pem";
+        NB_TLS_KEY_FILE = "/var/lib/acme/${netbirdDomain}/key.pem";
+      };
+      serviceConfig = {
+        EnvironmentFile = relaySecretEnv;
+        ExecStart = lib.getExe pkgs.netbird-relay;
+        Restart = "always";
 
-      # hardening
-      LockPersonality = true;
-      MemoryDenyWriteExecute = true;
-      NoNewPrivileges = true;
-      PrivateMounts = true;
-      ProtectClock = true;
-      ProtectControlGroups = true;
-      ProtectHome = true;
-      ProtectHostname = true;
-      ProtectKernelLogs = true;
-      ProtectKernelModules = true;
-      ProtectKernelTunables = true;
-      ProtectSystem = true;
-      RemoveIPC = true;
-      RestrictNamespaces = true;
-      RestrictRealtime = true;
-      RestrictSUIDSGID = true;
+        # hardening
+        LockPersonality = true;
+        MemoryDenyWriteExecute = true;
+        NoNewPrivileges = true;
+        PrivateMounts = true;
+        ProtectClock = true;
+        ProtectControlGroups = true;
+        ProtectHome = true;
+        ProtectHostname = true;
+        ProtectKernelLogs = true;
+        ProtectKernelModules = true;
+        ProtectKernelTunables = true;
+        ProtectSystem = true;
+        RemoveIPC = true;
+        RestrictNamespaces = true;
+        RestrictRealtime = true;
+        RestrictSUIDSGID = true;
+      };
+      stopIfChanged = false;
     };
-    stopIfChanged = false;
+    netbird-proxy = {
+      enable = true;
+      description = "The proxy service for Netbird, a wireguard VPN";
+      documentation = [ "https://netbird.io/docs/" ];
+      after = [
+        "network.target"
+        "netbird-management.service"
+      ];
+      wantedBy = [ "multi-user.target" ];
+      environment = {
+        NB_PROXY_DOMAIN = "proxy.moppu.dev";
+        NB_PROXY_MANAGEMENT_ADDRESS = "http://localhost:${toString config.services.netbird.server.management.port}";
+        NB_PROXY_ALLOW_INSECURE = "true";
+        NB_PROXY_ADDRESS = ":${toString proxyPort}";
+        NB_PROXY_ACME_CERTIFICATES = "true";
+        NB_PROXY_ACME_CHALLENGE_TYPE = "tls-alpn-01";
+        NB_PROXY_CERTIFICATE_DIRECTORY = "/var/lib/proxy-certs";
+        NB_PROXY_LOG_LEVEL = "info";
+      };
+      serviceConfig = {
+        EnvironmentFile = proxySecretEnv;
+        ExecStart = lib.getExe unstable.netbird-proxy;
+        Restart = "always";
+
+        # hardening
+        LockPersonality = true;
+        MemoryDenyWriteExecute = true;
+        NoNewPrivileges = true;
+        PrivateMounts = true;
+        ProtectClock = true;
+        ProtectControlGroups = true;
+        ProtectHome = true;
+        ProtectHostname = true;
+        ProtectKernelLogs = true;
+        ProtectKernelModules = true;
+        ProtectKernelTunables = true;
+        ProtectSystem = true;
+        RemoveIPC = true;
+        RestrictNamespaces = true;
+        RestrictRealtime = true;
+        RestrictSUIDSGID = true;
+      };
+      stopIfChanged = false;
+    };
   };
-
 }
